b620752418ec1580750feb824f53d9b74349b01b
Mario <Mario@192.168.40.104>
2018-04-06 12:07:29 -0300

    ahyapo

---
+++ Projectnew/src/projectnew/NuevaClase.java
@@ -0,0 +1,13 @@
+package projectnew;
+
+public class NuevaClase {
+
+    private int edad;
+    private String nombre;
+
+    public NuevaClase(int a, String b) {
+        edad = a;
+        nombre = b;
+    }
+
+}
